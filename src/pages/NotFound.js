a1726ed2e83d537de873a6fcc782e1249b5decfe
Éric  Colday NOUBISSI <54113095+eric-colday@users.noreply.github.com>
2022-07-25 13:34:24 +0200

    htacces et page 404 ajoutés

--- src/pages/NotFound.js
+++ src/pages/NotFound.js
@@ -1,10 +1,19 @@
 import React from 'react'
-import Navbar from '../components/Navbar'
+import { Link } from "react-router-dom";
+import Navbar from '../components/Navbar';
+
 
 export default function NotFound() {
   return (
-    <header className="header">
-      <Navbar />
-    </header>
+    <div>
+      <header className="header">
+        <Navbar/>
+      </header>
+      <main className="notFound404">
+        <p>404</p>
+        <p>Oups! La page que vous demandez n'existe pas.</p>
+        <Link to='/'>Retouner à la page d'accueil</Link>
+      </main>
+    </div>
   )
 }
